58f81d37c38a0ae8ee5d1cf7f5ef259a3375196e
beyzasenoll <beyzasenol08@gmail.com>
2024-04-26 22:01:04 +0300

    initial commit

---
+++ src/main/java/com/adviceapp/adviceapp/AdviceAppApplication.java
@@ -0,0 +1,13 @@
+package com.adviceapp.adviceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdviceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdviceAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adviceapp/adviceapp/AdviceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adviceapp.adviceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdviceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
