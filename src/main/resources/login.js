cab387156a88e64a757b3953bca2ba9633a17530
shamssannoufa <shamssannoufa719@gmail.com>
2024-05-21 17:10:56 +0300

    initial commit for front end

---
+++ src/main/resources/login.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const getStartedButton = document.getElementById("get-started-btn");
+
+    getStartedButton.addEventListener("click", function() {
+        const category = document.getElementById("category").value;
+        const duration = document.getElementById("duration").value;
+        // Burada seçilen kategori ve süreyi kullanarak gerekli işlemleri yapabilirsiniz
+        // Örneğin seçilen kategori ve süreye göre önerilen içerikleri getirebilir veya
+        // başka bir sayfaya yönlendirme yapabilirsiniz
+    });
+});
